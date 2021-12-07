ace4623d3ce8033af0681ee02daf9fc5d1644217
송지원 <wonji2017@yonsei.ac.kr>
2021-12-07 21:29:14 +0900

    chatList

--- routes/chatList.js
+++ routes/chatList.js
@@ -1,40 +1,41 @@
 //채팅 목록 
 var express = require('express');
 var router = express.Router();
+const { verifyMiddleWare } = require('../modules/jwt');
+
 
 /* GET /chatList/가 들어왔을 때 */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'chatLists' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'chatLists' });
+// });
 
 // 채팅 목록 검색
-// router.get('/chatList', verifyMiddleWare, async (req, res, next) => {
-//   const {id} = req.decoded;
+router.get('/chatList', verifyMiddleWare, async (req, res, next) => {
+  const {id} = req.decoded;
         
-//   const room_and_friend_ids = await query(
-//   `SELECT room_id, 
-//   (CASE 
-//     WHEN(user1_id = '${id}') THEN user2_id 
-//     WHEN(user2_id = '${id}') THEN user1_id END) friend_id
-//       FROM database05.room WHERE user1_id = '${id}' OR user2_id = '${id}'`);
+  const room_and_friend_ids = await query(
+  `SELECT room_id, 
+  (CASE 
+    WHEN(user1_id = '${id}') THEN user2_id 
+    WHEN(user2_id = '${id}') THEN user1_id END) friend_id
+      FROM database05.room WHERE user1_id = '${id}' OR user2_id = '${id}'`);
 
-//   const users = room_ids.map(room_friend_id => room_friend_id.room_id )
   
 
-//   try {
-//     res.json({
-//       status: 200,
-//       message: '친구 검색 성공',                   
-//       data: { user_self: me, users: users }             
-//     });
-//   } catch (error) {
-//     res.json({
-//       status:400,
-//       message: '친구 검색 실패',
-//       error
-//     });
-//   }
-// });
+  try {
+    res.json({
+      status: 200,
+      message: '친구 검색 성공',                   
+      data: { user_self: me, users: users }             
+    });
+  } catch (error) {
+    res.json({
+      status:400,
+      message: '친구 검색 실패',
+      error
+    });
+  }
+});
 
 module.exports = router;
 
--- routes/environ.js
+++ routes/environ.js
@@ -1,7 +1,6 @@
 //내 주변 //영어를... 뭐로 해야할 지 모르겠어서 environ에 인근이라는 뜻이 있길래 이걸로 했어요 
 var express = require('express');
 var router = express.Router();
-const { query } = require('../modules/db');
 const { verifyMiddleWare } = require('../modules/jwt');
 
 /* GET /environ/가 들어왔을 때 */
@@ -9,21 +8,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'environs' });
 });
 
-// router.get('/:place', verifyMiddleWare, async (req, res, next) => {
-//   const {place} = req.params
-//   const users = await query(`SELECT * FROM user WHERE place = '${place}'`)
+router.get('/:place', verifyMiddleWare, async (req, res, next) => {
+  const {place} = req.params
+  const users = await query(`SELECT * FROM user WHERE place = '${place}'`)
 
-//   if (users.length>0){
-//     res.json({
-//       status:200,
-//       data: users
-//     });
-//   } else {
-//     res.json({
-//       status:400,
-//       message : '사용자 목록 불러오기 실패'
-//     });
-//   }
-// });
+  if (users.length>0){
+    res.json({
+      status:200,
+      data: users
+    });
+  } else {
+    res.json({
+      status:400,
+      message : '사용자 목록 불러오기 실패'
+    });
+  }
+});
 
 module.exports = router;
